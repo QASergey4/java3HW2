069d4d062db7185898c08ad5504d5584bae31dd6
Sergey <springzepp@gmail.com>
2024-03-07 20:11:11 +0300

    Create program

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
     public int calculate(int massKG, float heightM) {
-        return (int) (massKG/Math.pow(heightM,2));
+        return (int) (massKG / Math.pow(heightM, 2));
     }
 }
